2ed605a52b622d645fec5f9970399f37e20f38ee
Chukwuemeka <chukwuemeka.o.ike@gmail.com>
2020-03-11 03:47:31 -0400

    New makefile

---
+++ cross.cpp
@@ -0,0 +1,20 @@
+#include <khepera/khepera.h>
+// #include <opencv2/opencv.hpp>
+// #include <opencv2/aruco.hpp>
+// #include <stdlib.h>
+#include <stdio.h>
+// #include <iostream>
+
+// using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int rc;
+
+  /* Set the libkhepera debug level - Highly recommended for development. */
+  kb_set_debug_level(2);
+
+  
+  printf("Hello, World!\n");
+  return 0;
+}
