db50de1d48321f467a3aea617d1c91c651855c5a
Chukwuemeka <Chukwuemeka.o.ike@gmail.com>
2020-03-24 18:51:18 -0400

    OpenCV built for Poky

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ The Aruco dictionary used by this project is DICT_4X4_50
 The tools in this repo employ the OpenCV and ArUco libraries in estimating the pose of a Khepera IV robot with respect to ArUco markers placed throughout the experimental space.
 
 The functionality was developed by Chukwuemeka Ike to aid Ruixuan Yan in his experiments which required that a swarm of robots be able to find their collective centroid.
+
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules/aruco -DOPENCV_ENABLE_CXX11=OFF -DSOFTFP=ON -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_TOOLCHAIN_FILE=../arm-gnueabi.toolchain.cmake -DBUILD_OPENCV_PYTHON=OFF ../opencv
+
+
--- cross.cpp
+++ cross.cpp
@@ -1,11 +1,12 @@
-#include <khepera/khepera.h>
-// #include <opencv2/opencv.hpp>
-// #include <opencv2/aruco.hpp>
-// #include <stdlib.h>
+#include "khepera/khepera.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <stdlib.h>
 #include <stdio.h>
-// #include <iostream>
+#include <iostream>
 
-// using namespace std;
+using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +15,36 @@ int main(int argc, char *argv[])
   /* Set the libkhepera debug level - Highly recommended for development. */
   kb_set_debug_level(2);
 
-  
+
   printf("Hello, World!\n");
+
+  cv::Mat inputImage, imageCopy;
+  inputImage = imread("./test images/image02.jpg");
+
+
+  // cv::Mat cameraMatrix, distCoeffs;
+  // readCameraParameters(cameraMatrix, distCoeffs);
+
+  std::vector<int> markerIds;
+  std::vector<std::vector<cv::Point2f> > markerCorners, rejectedCandidates;
+  cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
+  cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+  cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
+
+  inputImage.copyTo(imageCopy);
+
+  // if at least one marker detected
+  if (markerIds.size() > 0){
+    cv::aruco::drawDetectedMarkers(imageCopy, markerCorners, markerIds);
+
+    // std::vector<cv::Vec3d> rvecs, tvecs;
+    // cv::aruco::estimatePoseSingleMarkers(markerCorners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+    // draw axis for each marker
+    // for(int i=0; i<markerIds.size(); i++){
+    //   cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+    // }
+  }
+
   return 0;
 }
